2374ad1e4dc9d5bbcdf997d7e7be8493f05ebb3f
Michael Lodder <mike@litprotocol.com>
2024-01-05 10:28:20 -0700

    finalize changes to work with taproot

--- frost-core/src/round2.rs
+++ frost-core/src/round2.rs
@@ -6,7 +6,7 @@ use crate as frost;
 use crate::{
     Challenge, Ciphersuite, Error, Field, Group, {round1, *},
 };
-// use subtle::ConditionallyNegatable;
+use subtle::ConditionallyNegatable;
 
 /// A participant's signature share, which the coordinator will aggregate with all other signer's
 /// shares into the joint signature.
@@ -109,12 +109,17 @@ pub(super) fn compute_signature_share<C: Ciphersuite>(
     lambda_i: <<<C as Ciphersuite>::Group as Group>::Field as Field>::Scalar,
     key_package: &keys::KeyPackage<C>,
     challenge: Challenge<C>,
+    y_is_odd: subtle::Choice,
 ) -> SignatureShare<C> {
-    let z_share: <<C::Group as Group>::Field as Field>::Scalar = signer_nonces.hiding.to_scalar()
-        + (signer_nonces.binding.to_scalar() * binding_factor.0)
-        + (lambda_i * key_package.signing_share.to_scalar() * challenge.to_scalar());
+    let mut nonce = signer_nonces.hiding.0 + (signer_nonces.binding.0 * binding_factor.0);
+    nonce.conditional_negate(y_is_odd);
+    let mut signing_share = key_package.signing_share.0;
+    signing_share.conditional_negate(<C::Group>::y_is_odd(&key_package.verifying_key.element));
 
-    SignatureShare::<C>::new(z_share)
+    let z_share: <<C::Group as Group>::Field as Field>::Scalar =
+        nonce + (lambda_i * signing_share * challenge.0);
+
+    SignatureShare::<C> { share: z_share }
 }
 
 /// Performed once by each participant selected for the signing operation.
@@ -182,7 +187,7 @@ pub fn sign<C: Ciphersuite>(
         lambda_i,
         &key_package,
         challenge,
-        // <C::Group>::y_is_odd(&group_commitment.0),
+        <C::Group>::y_is_odd(&group_commitment.0),
     );
 
     Ok(signature_share)
