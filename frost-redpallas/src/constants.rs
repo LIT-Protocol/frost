9d09932d4582b8b3e51882b8c977833b5e136733
Michael Lodder <mike@litprotocol.com>
2025-11-11 15:23:10 -0700

    add pallas
    Signed-off-by: Michael Lodder <mike@litprotocol.com>

---
+++ frost-redpallas/src/constants.rs
@@ -0,0 +1,24 @@
+// -*- mode: rust; -*-
+//
+// This file is part of reddsa.
+// Copyright (c) 2019-2021 Zcash Foundation
+// See LICENSE for licensing information.
+//
+// Authors:
+// - Henry de Valence <hdevalence@hdevalence.ca>
+
+// /// The byte-encoding of the basepoint for `SpendAuthSig`.
+// // Extracted ad-hoc from librustzcash
+// // XXX add tests for this value.
+pub const SPENDAUTHSIG_BASEPOINT_BYTES: [u8; 32] = [
+    99, 201, 117, 184, 132, 114, 26, 141, 12, 161, 112, 123, 227, 12, 127, 12, 95, 68, 95, 62, 124,
+    24, 141, 59, 6, 214, 241, 40, 179, 35, 85, 183,
+];
+
+// /// The byte-encoding of the basepoint for `BindingSig`.
+// // Extracted ad-hoc from librustzcash
+// // XXX add tests for this value.
+// pub const BINDINGSIG_BASEPOINT_BYTES: [u8; 32] = [
+//     139, 106, 11, 56, 185, 250, 174, 60, 59, 128, 59, 71, 176, 241, 70, 173, 80, 171, 34, 30, 110,
+//     42, 251, 230, 219, 222, 69, 203, 169, 211, 129, 237,
+// ];
